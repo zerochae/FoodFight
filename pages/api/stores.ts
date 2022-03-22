13fe5aabffe4909edac4ac5a72f202def25fedf9
zerochae <zerochae@kakao.com>
2022-03-22 19:37:06 +0900

    Fix: api handler

--- pages/api/stores.ts
+++ pages/api/stores.ts
@@ -2,7 +2,7 @@ import { NextApiHandler } from "next";
 import db from "db.json";
 
 const handler: NextApiHandler = (req, res) => {
-  res.status(200).json(db);
+  res.status(200).json(db.stores);
 };
 
 export default handler;
